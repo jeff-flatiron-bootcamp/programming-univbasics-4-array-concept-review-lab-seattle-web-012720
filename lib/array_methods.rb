13538e1e303e5eedb3014bcf45cc632822cbd370
Jeff Simon <jeffsbillinfo@gmail.com>
2020-01-08 04:19:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -35,7 +35,7 @@ def find_min_value(array)
   #array.sort!
   #array[0]
   
-    ##.times looping
+  ##.times looping
   min = array[0]
   ((array.length)-1).times do |indexer|
     if min > array[indexer+1]

4a2a0bb502528145ab744ad0d5b33b0dbdcd4d10
Jeff Simon <jeffsbillinfo@gmail.com>
2020-01-08 04:16:50 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -18,8 +18,7 @@ def find_max_value(array)
   
   counter = 0
   max = array[0]
-  while array[counter]
-  do
+  while array[counter] do
     if max < array[counter]
       max = array[counter]
     end
